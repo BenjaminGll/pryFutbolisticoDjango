4e5c39f0ba28f56ee590d226d78980f03f9d97c2
iosivrioja <iosiv120402@gmail.com>
2023-11-01 16:35:01 -0500

    volver

--- appArbitro/admin.py
+++ appArbitro/admin.py
@@ -3,15 +3,27 @@ from appArbitro.models import *
 
 # Register your models here.
 
-class tipoArbitroAdmin(admin.ModelAdmin):
-    list_display=['arbitro_id','nombre_terna','estado']
+class arbitroAdmin(admin.ModelAdmin):
+    list_display=['arbitro_id','nombre','apellido','tipo_arbitro','pais_id','estado']
     ordering=['arbitro_id']
+    search_fields = ['nombre','apellido']
+
+class tipoTernaAdmin(admin.ModelAdmin):
+    list_display=['tipo_terna_id','descripcion','siglas']
+    ordering=['tipo_terna_id']
+    search_fields = ['descripcion','siglas']
+
+class ternaArbitralAdmin(admin.ModelAdmin):
+    list_display=['terna_arbitral_id','nombre_terna','estado']
+    ordering=['terna_arbitral_id']
     search_fields = ['nombre_terna']
 
-class detalleTernaAdmin(admin.ModelAdmin):
-    list_display=['detalle_terna_id','tipo_arbitro_id','encuentro_id', 'persona_id']
+class detalleTernaArbitralAdmin(admin.ModelAdmin):
+    list_display=['detalle_terna_id','terna_arbitral_id','arbitro_id','tipo_terna_id','estado_juego']
     ordering=['detalle_terna_id']
-    search_fields = ['tipo_arbitro_id']
+    search_fields = ['terna_arbitral_id','arbitro_id']
 
-admin.site.register(tipo_arbitro,tipoArbitroAdmin)
-admin.site.register(detalle_terna,detalleTernaAdmin)
+admin.site.register(arbitro,arbitroAdmin)
+admin.site.register(tipo_terna,tipoTernaAdmin)
+admin.site.register(terna_arbitral,ternaArbitralAdmin)
+admin.site.register(detalle_terna,detalleTernaArbitralAdmin)

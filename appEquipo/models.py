456eb56978809310aad93a7c61280b0c75d79b3d
Frank'O <Frank Ocrospoma Ugaz>
2023-11-11 21:14:00 -0500

    Corrijiendo wvds ps

--- appEquipo/models.py
+++ appEquipo/models.py
@@ -103,13 +103,14 @@ class posicion_jugador(models.Model):
 #     class Meta:
 #         verbose_name_plural='alineacion_equipo'
 class alineacion(models.Model):
+    descripcion_encuentro_id=models.ForeignKey('appPartido.descripcion_encuentro',on_delete=models.CASCADE,db_column='descripcion_encuentro_id',related_name='descripcion_encuentros')
+    contrato_id=models.ForeignKey('appContrato.contrato',on_delete=models.CASCADE,db_column='contrato_id', null=True)
     alineacion_id=models.BigAutoField(primary_key=True)
     dorsal=models.IntegerField(null=True)
     posicion_jugador_id=models.ForeignKey(posicion_jugador,on_delete=models.CASCADE,db_column='posicion_jugador_id',null=True)
     capitan=models.BooleanField(null=True)
     estado=models.BooleanField(null=True)
-    descripcion_encuentro_id=models.ForeignKey('appPartido.descripcion_encuentro',on_delete=models.CASCADE,db_column='descripcion_encuentro_id',related_name='descripcion_encuentros')
-    contrato_id=models.ForeignKey('appContrato.contrato',on_delete=models.CASCADE,db_column='contrato_id', null=True)
+    
     
     def __str__(self):
         return str(self.contrato_id)

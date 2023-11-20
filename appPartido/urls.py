0eb741991c133c89de028bad76e646d861767d0f
ZCuak <alex_geancarlos@hotmail.com>
2023-11-20 07:48:16 -0500

    Prueba

--- appPartido/urls.py
+++ appPartido/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('asignar/alineaciones/<int:encuentro_id>/', asignarAlineacion, name='asignarAlineaciones'),
     path('asignar/eventos/<int:encuentro_id>/', asignarEventos, name='asignarEventos'),
     path('asignar/<str:tipo>/<int:encuentro_id>/', asignar, name='asignar'),
-    # path('asignar/estadisticas/<int:encuentro_id>/', asignarEstadisticas, name='asignarEstadisticas'),
+    path('asignar/estadisticas/<int:encuentro_id>/', asignarEstadisticas, name='asignarEstadisticas'),
 
 
 

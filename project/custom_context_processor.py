6cb85d16b4c9f5dc6990ab824ffa8b8bf8ef298e
Frank'O <Frank Ocrospoma Ugaz>
2023-11-24 22:20:46 -0500

    Corrijiendo wvds8

--- project/custom_context_processor.py
+++
@@ -1,8 +0,0 @@
-from appCompeticion.models import deporte,competicion
-
-def listar_deportes(request):
-
-    return {
-       'deportes_nav': deporte.objects.all(),
-       'competencias_nav': competicion.objects.all(),
-    }

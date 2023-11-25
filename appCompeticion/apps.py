287630ad25c87ca4a53eaee62113124c7966e554
Frank'O <Frank Ocrospoma Ugaz>
2023-11-24 21:56:32 -0500

    Corrijiendo wvds8

--- appCompeticion/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppcompeticionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appCompeticion'
-    verbose_name='App Competiciones'
-    icon='fa fa-trophy'

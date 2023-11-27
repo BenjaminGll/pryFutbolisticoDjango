e04089e9968d9e5843df73e35c18ac49cca758a7
CinthiaSanchezC <cinthiasanchezcieza@gmail.com>
2023-11-27 02:04:13 -0500

    eli

--- static/assets/js/editar_botones.js
+++ static/assets/js/editar_botones.js
@@ -15,30 +15,18 @@
         table.find('tbody tr').each(function(){
             var row = $(this);
             var editUrl = row.find('th a').attr('href'); // Obtener el enlace de edición desde la primera celda (ID)
-
+            var deletetUrl = row.find('th a').attr('href'); // Obtener el enlace de edición desde la primera celda (ID)
             // Crear el botón de editar con un ícono de lápiz de FontAwesome
             var editButton = $('<a/>').attr({ href: editUrl }).html('<i class="fas fa-pencil-alt"></i>'); // Ajusta la clase del ícono según FontAwesome
             var editCell = $('<td/>').append(editButton);
 
             // Crear el botón de eliminar con un ícono de basura de FontAwesome
-            var deleteButton = $('<a/>').attr({ href: '#' }).html('<i class="fas fa-trash"></i>'); // Ajusta la clase del ícono según FontAwesome
+            var deleteButton = $('<a/>').attr({ href: deletetUrl }).html('<i class="fas fa-trash"></i>'); // Ajusta la clase del ícono según FontAwesome
             var deleteCell = $('<td/>').append(deleteButton);
 
             // Agregar la celda de editar y eliminar a la fila
             row.append(editCell, deleteCell);
 
-            // Agregar la lógica de eliminación al botón de eliminar
-            deleteButton.on('click', function(e){
-                e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-
-                // Aquí puedes agregar la lógica de eliminación, por ejemplo, mostrar un cuadro de diálogo de confirmación
-                var confirmDelete = confirm('¿Estás seguro de que quieres eliminar este elemento?');
-                if(confirmDelete){
-                    // Simular la acción de marcado de casilla de verificación y enviar el formulario de eliminación masiva
-                    row.find('td input[type="checkbox"]').prop('checked', true);
-                    $('#changelist-form').submit();
-                }
-            });
         });
     });
 })(django.jQuery);

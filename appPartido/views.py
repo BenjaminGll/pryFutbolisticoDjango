c1565b5e2c1c92edf6b5c9058cf17d295ea9846f
IvannaCST <IvannaTavara09@gmail.com>
2023-11-26 20:21:14 -0500

    reporte

--- appPartido/views.py
+++ appPartido/views.py
@@ -290,15 +290,11 @@ def asignarEventos(request, encuentro_id):
         print('equipo',evento_equipo)
         if tipo_evento_id == '3':
             if evento_equipo == 'Local':
-                alineacion01 = alineacion.objects.filter(
-                descripcion_encuentro_id__in=descripcionEncuentroLocal_objs)
-                alineacion02 = alineacion.objects.filter(
-                descripcion_encuentro_id__in=descripcionEncuentroLocal_objs)
+                alineacion01 = alineacion.objects.filter(descripcion_encuentro_id__in=descripcionEncuentroLocal_objs, estado=True)
+                alineacion02 = alineacion.objects.filter(descripcion_encuentro_id__in=descripcionEncuentroLocal_objs, estado=False)
             elif evento_equipo == 'Visita':
-                alineacion01 = alineacion.objects.filter(
-                descripcion_encuentro_id__in=descripcionEncuentroVisita_objs)
-                alineacion02 = alineacion.objects.filter(
-                descripcion_encuentro_id__in=descripcionEncuentroVisita_objs)
+                alineacion01 = alineacion.objects.filter(descripcion_encuentro_id__in=descripcionEncuentroVisita_objs, estado=True)
+                alineacion02 = alineacion.objects.filter(descripcion_encuentro_id__in=descripcionEncuentroVisita_objs, estado=False)
             else:
                 alineacion01 = alineacion.objects.filter(
                 descripcion_encuentro_id__in=descripcionEncuentroLocal_objs)

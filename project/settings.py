8dd621308031a5b92329913a10ac21b55aa8bc1f
Frank Ocrospoma Ugaz <frankocrospomaugaz@gmail.com>
2023-12-09 19:10:51 -0500

    Corrijiendo wvds8

--- project/settings.py
+++ project/settings.py
@@ -107,6 +107,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'BDFUT',
+#         'USER': 'postgres',
+#         'PASSWORD': 'frank2003',
+#         'HOST': 'localhost',  # si tienes otra dirección host debes remplazar esta
+#         'PORT': '5432',  # si lo dejas vacío tomará el puerto por default
+#     }
+# }
+
 
 
 # Password validation
